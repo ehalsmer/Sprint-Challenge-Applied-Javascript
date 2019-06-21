1c520dfaa3c8b903e1f71e88f29549e84efdd884
Eleasah Halsmer <eleasahjh@gmail.com>
2019-06-21 10:52:39 -0600

    wip: image carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,7 +2,9 @@ class Carousel {
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+let left = document.querySelector('.carousel .left-button');
+let right = document.querySelector('.carousel .right-button');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
